806782ece35d6117de03212e3a25d9f3741cdfa8
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2025-03-16 12:05:17 +0800

    Add user, upload_avator and get_image functions

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, jsonify, make_response
+from flask import Flask, request, jsonify, make_response, send_from_directory
 from flask_cors import CORS
 from models import db, init_db, User
 import random
@@ -11,11 +11,7 @@ import os
 load_dotenv()
 
 app = Flask(__name__)
-
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
-
-print("Database URI:", app.config["SQLALCHEMY_DATABASE_URI"])
-
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 init_db(app)
@@ -23,6 +19,9 @@ init_db(app)
 @app.route('/status')
 def status():
     return jsonify({"status": "ok"})
+
+def generate_reset_token(length):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
 
 @app.route('/register', methods=['POST'])
 def register():
@@ -41,7 +40,7 @@ def register():
     new_user = User(
         username=username,
         email=email,
-        key_certificate=''.join(random.choices(string.ascii_letters + string.digits, k=30))
+        key_certificate= generate_reset_token(30)
     )
     new_user.set_password(password)
 
@@ -81,6 +80,82 @@ def login():
     response.set_cookie("user_session", username, httponly=True, secure=True, samesite="Strict")
     response.set_cookie("role", user.role, httponly=True, secure=True, samesite="Strict")
     return response
+
+@app.route('/logout')
+def logout():
+    response = make_response(jsonify({"message": "登出成功"}))
+    response.set_cookie("user_session", "", expires=0)
+    return response
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    username = request.cookies.get("user_session", "").strip()
+    user = User.query.filter_by(username=username).first()
+    role = user.role
+
+    if not role or not username:
+        return jsonify({"error": "未授權"}), 401
+
+    if role == "admin":
+        users = User.query.with_entities(User.id, User.username, User.last_login, User.login_count, User.role).all()
+    elif role == "user":
+        users = User.query.with_entities(User.id, User.username, User.last_login, User.login_count, User.role).filter_by(username=username).all()
+    else:
+        return jsonify({"error": "無法識別角色"}), 403
+
+    return jsonify([user._asdict() for user in users])
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/upload-avatar', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return jsonify({"error": "請提供照片"}), 400
+
+    file = request.files['file']
+    username = request.cookies.get("user_session", "").strip()
+    if not file or not username:
+        return jsonify({"error": "請提供帳號與照片"}), 400
+
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({"error": "使用者不存在"}), 404
+
+    filename = f"{user.id}.jpg"
+    filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    file.save(filepath)
+
+    user.profile_image = filepath
+    user.picture_name = filename
+    db.session.commit()
+
+    return jsonify({"message": "照片上傳成功", "file_path": filepath})
+
+@app.route('/get_user_image', methods=['GET'])
+def get_user_image():
+    username = request.cookies.get("user_session", "").strip()
+
+    if not username:
+        return jsonify({"error": "未授權"}), 401
+
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        return jsonify({"error": "用戶未找到"}), 404
+
+    if user.profile_image:
+        image_path = os.path.join(app.config['UPLOAD_FOLDER'], user.profile_image)
+        if os.path.exists(image_path):
+            try:
+                return send_from_directory(app.config['UPLOAD_FOLDER'], user.picture_name, as_attachment=True)
+            except Exception as e:
+                return jsonify({"error": f"檔案讀取錯誤: {str(e)}"}), 500
+        else:
+            return jsonify({"error": "圖片檔案不存在"}), 404
+
+    return jsonify({"error": "無圖片可顯示"}), 404
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
