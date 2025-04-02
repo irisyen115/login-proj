d565b13ec972dae4c7808210f18055c215740993
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2025-04-02 14:45:01 +0800

    Change some config names, detailed some code to service and controllers and add the user_key function back

--- backend/app/controllers/user_controller.py
+++ backend/app/controllers/user_controller.py
@@ -1,6 +1,5 @@
 from flask import Blueprint, jsonify, g
-from models import User
-from services.user_service import get_user_by_id
+from services.user_service import fetch_users_data
 
 user_bp = Blueprint("user", __name__)
 
@@ -9,15 +8,8 @@ def get_users():
     if not g.get("user_id"):
         return jsonify({"error": "未授權"}), 401
 
-    user = get_user_by_id(g.user_id)
-    if not user:
-        return jsonify({"error": "使用者不存在"}), 404
+    user = fetch_users_data(g.user_id)
+    if user and "error" in user:
+        return jsonify(user), 404
 
-    if user.role == "admin":
-        users = User.query.with_entities(User.id, User.username, User.last_login, User.login_count, User.role).all()
-        users_data = [user_row._asdict() for user_row in users]
-        return jsonify(users_data)
-    elif user.role == "user":
-        user_list = [user]
-        users_data = [user_col.to_dict() for user_col in user_list]
-        return jsonify(users_data)
+    return jsonify(user)
